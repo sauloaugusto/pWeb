e401b66fc091d4e1226ca5986e0103273aca811c
Saulo <saulojordao@gmail.com>
2020-04-21 21:16:54 -0300

    Add files via upload

--- src/main/java/br/com/saulo/testegit/App.java
+++ src/main/java/br/com/saulo/testegit/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+    int n = 10;
+    
+    for (int i = 0; i < n ; i++) {
+    	System.out.println(i);
+    }
+    
+    
     }
 }
